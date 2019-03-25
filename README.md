76da3333624b6045d733d2f3c13b91c8d4cfd12e
Shaun Smith <shaun.smith@oracle.com>
2019-03-25 16:04:10 -0400

    Consolidate code and added Readme.

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-## Example showing how to call an Oracle Function by its invoke endpoint using Node.js
+# Invoke Oracle Functions by its Endpoint
+
+This example demonstrates how to invoke a function deployed to Oracle Functions
+by its invoke endpoint URL.
+
+## Pre-requisites
+
+1. Install/update the Fn CLI
+
+   `curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install |
+   sh`
+
+2. Setup your OCI config file as described in the Oracle Functions quick start guide.
+
+3. Create a function to invoke.  You can follow these instructions for a [Node
+   Hello World
+   Function](https://github.com/abhirockzz/oracle-functions-hello-worlds/blob/master/node-hello-world.md)
+   if you don't already have one.
+
+## Setup
+
+1. Clone this repository
+
+   `git clone https://github.com/shaunsmith/fn-node-invokebyendpoint.git`
+
+2. Cd into the directory where you cloned the example:
+
+   `cd fn-node-invokebyendpoint`
+
+3. Install the required Node modules:
+
+    `npm install`
+
+4. Copy `config.template` to `config.yaml` and add your OCI details.  (NOTE: If
+   you've configured the fn CLI to work with Oracle Functions you can copy
+   values from your `~/.oci/config` and fn context file).
+
+    ```yaml
+    tenancyId: <ocid>
+    compartmentId: <ocid>
+    userId: <ocid>
+    keyFingerprint:
+    privateKeyPath:
+    passphrase:
+    ```
+
+5. Inspect a function to get its invoke endpoint.  For example, if you have a
+   function "hello" in the "quickstart" application, you can inspect the function
+   with:
+
+    `fn inspect f quickstart hello`
+
+   This will return JSON similar to the following:
+
+   ```json
+   {
+     "annotations": {
+       "fnproject.io/fn/invokeEndpoint": "https://iagu5qcq6iq.us-phoenix-1.functions.oci.oraclecloud.com/20181201/functions/ocid1.fnfunc.oc1.phx.aaaaaaaaac4lertr6bwpulbgqogsv753afil4zytaazomqxlebdfov2isnoq/actions/invoke",
+       "oracle.com/oci/compartmentId": "ocid1.compartment.oc1..aaaaaaaaokbzj2jn3hf5kwdwqoxl2dq7u54p3tsmxrjd7s3uu7x23tkegiua"
+     },
+     "app_id": "ocid1.fnapp.oc1.phx.aaaaaaaaafqfe5nuqtxw4lyjf3wf7vbzwqzmcll3pf3bztcwviagu5qcq6iq",
+     "created_at": "2019-03-21T16:33:46.989Z",
+     "id": "ocid1.fnfunc.oc1.phx.aaaaaaaaac4lertr6bwpulbgqogsv753afil4zytaazomqxlebdfov2isnoq",
+     "idle_timeout": 30,
+     "image": "phx.ocir.io/oracle-serverless-devrel/shaunsmith/hello:0.0.9",
+     "memory": 128,
+     "name": "hello",
+     "timeout": 30,
+     "updated_at": "2019-03-21T18:16:27.729Z"
+   }
+   ```
+
+6. You can now invoke the function using the `invokefunc.js` with its endpoint
+   copied from the `fnproject.io/fn/invokeEndpoint` property.  The command syntax is:
+
+   `node invokefunc.js <invoke endpoint url>`
+
+   For example using the invoke endpoint for our `hello` function:
+
+   `node invokefunc.js https://iagu5qcq6iq.us-phoenix-1.functions.oci.oraclecloud.com/20181201/functions/ocid1.fnfunc.oc1.phx.aaaaaaaaac4lertr6bwpulbgqogsv753afil4zytaazomqxlebdfov2isnoq/actions/invoke`
+
+## Understanding `invokefunc.js`
+
+The `invokefunc.js` utility is based on the [OCI Node.js request signing
+example](https://docs.cloud.oracle.com/iaas/Content/API/Concepts/signingrequests.htm#NodeJS).
+The only difference this code and the signing example is the addition of code to
+decrypt the private key using a passphrase.  The changes are noted in the code.
--- invokefunc.js
+++ invokefunc.js
@@ -1,39 +1,82 @@
-const fs = require('fs');
-const yaml = require('yamljs');
-const sign = require('./ocisign').sign;
-const URL = require('url').URL;
-const https = require('https');
+const fs = require('fs')
+const os = require('os')
+const yaml = require('yamljs')
+const URL = require('url').URL
+const https = require('https')
+const jsSHA = require('jssha')
+const sshpk = require('sshpk')
+const httpSignature = require('http-signature')
+
+// signing function as described at https://docs.cloud.oracle.com/Content/API/Concepts/signingrequests.htm
+// BUT with 2 changes to support private keys encrypted with a passphrase
+function sign (request, options) {
+  var keyId = options.tenancyId + '/' + options.userId + '/' + options.keyFingerprint
+  // 1. Decrypt the private key using the passphrase
+  let key = sshpk.parsePrivateKey(options.privateKey, 'auto', { passphrase: options.passphrase })
+
+  var headersToSign = [
+    'host',
+    'date',
+    '(request-target)'
+  ]
+
+  var methodsThatRequireExtraHeaders = ['POST', 'PUT']
+
+  if (methodsThatRequireExtraHeaders.indexOf(request.method.toUpperCase()) !== -1) {
+    options.body = options.body || ''
+
+    var shaObj = new jsSHA('SHA-256', 'TEXT')
+    shaObj.update(options.body)
+
+    request.setHeader('Content-Length', options.body.length)
+    request.setHeader('x-content-sha256', shaObj.getHash('B64'))
+
+    headersToSign = headersToSign.concat([
+      'content-type',
+      'content-length',
+      'x-content-sha256'
+    ])
+  }
+
+  httpSignature.sign(request, {
+    key: key.toBuffer('pem', {}), // 2. Format the decrypted Key as pem
+    keyId: keyId,
+    headers: headersToSign
+  })
+
+  var newAuthHeaderValue = request.getHeader('Authorization').replace('Signature ', 'Signature version="1",')
+  request.setHeader('Authorization', newAuthHeaderValue)
+}
 
 // generates a function to handle the https.request response object
-function handleRequest(callback) {
-
+function handleRequest (callback) {
   return function (response) {
-    var responseBody = "";
+    var responseBody = ''
 
     response.on('data', function (chunk) {
-      responseBody += chunk;
-    });
+      responseBody += chunk
+    })
 
     response.on('end', function () {
-      callback(responseBody);
-    });
+      callback(responseBody)
+    })
   }
 }
 
-// Call the specified function invoke endpoint signing the request 
-function invokeFunction(ctx, functionInvokeURL, body, callback) {
-  const url = new URL(functionInvokeURL);
+// Call the specified function invoke endpoint signing the request
+function invokeFunction (ctx, functionInvokeURL, body, callback) {
+  const url = new URL(functionInvokeURL)
   var options = {
     host: url.hostname,
     method: 'POST',
     path: url.pathname,
     headers: {
-      "opc-compartment-id": ctx.compartmentId,
-      "Content-Type": "application/text"
+      'opc-compartment-id': ctx.compartmentId,
+      'Content-Type': 'application/text'
     }
-  };
+  }
 
-  var request = https.request(options, handleRequest(callback));
+  var request = https.request(options, handleRequest(callback))
 
   sign(request, {
     body: body,
@@ -42,30 +85,33 @@ function invokeFunction(ctx, functionInvokeURL, body, callback) {
     tenancyId: ctx.tenancyId,
     userId: ctx.userId,
     passphrase: ctx.passphrase
-  });
+  })
 
-  request.write(body);
-  request.end();
-};
-
+  request.write(body)
+  request.end()
+}
 
 if (!process.argv[2]) {
-  console.log("usage: node " + process.argv[1] + " <function invoke endpoint>")
-  process.exit(-1);
+  console.log('usage: node ' + process.argv[1] + ' <function invoke endpoint>')
+  process.exit(-1)
 }
 
-var fnInvokeEndpoint = process.argv[2];
-var context = yaml.load('config.yaml');
-var keyPath = context.privateKeyPath;
-if (keyPath.indexOf("~/") === 0) {
-  keyPath = keyPath.replace("~", os.homedir());
+var fnInvokeEndpoint = process.argv[2]
+var context = yaml.load('config.yaml') // load OCI context values
+var keyPath = context.privateKeyPath
+if (keyPath.indexOf('~/') === 0) {
+  keyPath = keyPath.replace('~', os.homedir())
 }
-context.privateKey = fs.readFileSync(keyPath, 'ascii');
 
-//TODO: add support for function body
-invokeFunction(context, fnInvokeEndpoint, "", function (response) {
-  console.log(response);
+// read the private key
+fs.readFile(keyPath, 'ascii', (err, data) => {
+  if (err) {
+    console.error("Can't read keyfile: " + keyPath)
+    process.exit(-1)
+  }
+  context.privateKey = data
+  var body = '' // TODO: add support for function body
+  invokeFunction(context, fnInvokeEndpoint, body, function (response) {
+    console.log(response)
+  })
 })
-
-
-
